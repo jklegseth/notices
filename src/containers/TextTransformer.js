59c7c6c399fb810ef921a27da9a225c5b6515d8d
Junior Klegseth <junior.klegseth@continued.com>
2019-07-11 19:45:30 -0700

    Convert to component to use bindActionCreators and get access to dispatch; add dispatches to reset results and toast

--- src/containers/TextTransformer.js
+++ src/containers/TextTransformer.js
@@ -1,6 +1,23 @@
+import React from 'react'
+import { bindActionCreators } from 'redux'
 import { connect } from 'react-redux'
 import TextTransformer from '../components/TextTransformer'
 import { transformToLowerCase, transformToUpperCase } from '../store/textTransform'
+
+class TextTransformerContainer extends React.Component {
+    componentWillUnmount() {
+        // need to clear this or it persists to the other transform page
+        this.props.dispatch({ type: 'TRANSFORM_VALUE_RESET' })
+        // this belongs at the App level on route change
+        this.props.dispatch({ type: 'HIDE_TOAST' })
+    }
+
+    render() {
+        return (
+            <TextTransformer { ...this.props } />
+        )
+    }
+}
 
 const mapStateToProps = state => ({
     transformedValue: state.textTransform.transformedValue,
@@ -9,4 +26,11 @@ const mapStateToProps = state => ({
     hasError: !!state.textTransform.error
 })
 
-export default connect(mapStateToProps, { transformToLowerCase, transformToUpperCase })(TextTransformer)
+function mapDispatchToProps(dispatch) {
+    return {
+        dispatch,
+        ...bindActionCreators({ transformToLowerCase, transformToUpperCase }, dispatch)
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(TextTransformerContainer)
